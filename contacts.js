1562b62bd6b04844b233297be7bb97da9f1d4a3a
Unknown <jeremy.falaschi@gmail.com>
2018-07-20 19:01:48 +0200

    tp

--- contacts.js
+++ contacts.js
@@ -11,7 +11,6 @@ var Contact = {
         this.nom = nom;
         this.prenom = prenom;
     }
-    
 
 };
 
@@ -27,8 +26,6 @@ contacts.push(contact2);
 
 var gestionnaire = ["1 : Lister les contacts", "2 : Ajouter un contact", "0 : Quitter"];
 
-
-
 console.log("bienvenue dans le gestionnaire des contacts !");
 
 for (var i = 0; i < gestionnaire.length; i++){
@@ -37,44 +34,31 @@ for (var i = 0; i < gestionnaire.length; i++){
 var option = Number(prompt("Choisissez une option :"));
 while(option != 0){
     switch (option){
-
         case 1:
-
             console.log("Voici la liste de tous vos contacts :")
-
             contacts.forEach(function(contact){
                 console.log("Nom : " + contact.nom + ", prénom : " + contact.prenom);   
             });
-
             for (var i = 0; i < gestionnaire.length; i++){
                 console.log(gestionnaire[i]);
             };
-
             option = Number(prompt("Choisissez une option :"));
             break;
-
-
         case 2:
-            var nomDuContact = prompt("Entrez un nom :");
-            var prenomDuContact = prompt("Entrez un prenom :");
-
-            contacts.push(nomDuContact, prenomDuContact); 
-
+            var nom = prompt("Entrez un nom :");
+            var prenom = prompt("Entrez un prenom :");
+            var contactTemp = Object.create(Contact);
+            contactTemp.init(nom, prenom);
+            contacts.push(contactTemp);
             console.log("Le nouveau contact a été ajouté");
-
             for (var i = 0; i < gestionnaire.length; i++){
                 console.log(gestionnaire[i]);
-            };
-
+            }
             option = Number(prompt("Choisissez une option :"));
             break;
-
-
-
         default:
             console.log("Je n'ai pas compris !");
             option = Number(prompt("Choisissez une option :"));
-
     }
 }
 
